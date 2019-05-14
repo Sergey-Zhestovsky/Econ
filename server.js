86c4c3bee9b8d48934a868b0d55ba6b8be84bc97
Sergey Zhestovsky <Sergey.play123@gmail.com>
2019-05-14 10:01:09 +0300

    configuration initial settings

---
+++ server.js
@@ -0,0 +1,31 @@
+let express = require("express"),
+  http = require('http'),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  config = require("./config"),
+  port = process.env.PORT || config.port;
+
+let entryRouter = require("./routes/entry"),
+  
+  errorRouter = require("./routes/error");
+
+let app = express();
+
+http = http.Server(app);
+app.set("dir", __dirname);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.all("*", entryRouter);
+//app.use("/projects", projectsRouter);
+//app.use("/authorization", authRouter);
+
+app.use(errorRouter.error);
+app.use(errorRouter.devError);
+
+http.listen(port, () => {
+  console.log(`Listening on port ${port}\nhttp://localhost:${port}`);
+});
